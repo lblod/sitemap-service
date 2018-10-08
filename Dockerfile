985f711754568cbd29e0f7e0e038aa9c47f4f894
Niels Vandekeybus <progster@gmail.com>
2018-10-08 09:27:00 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM semtech/mu-javascript-template:1.3.2
+LABEL maintainer="info@redpencil.io"
+
---
+++ app.js
@@ -0,0 +1,55 @@
+import fs from 'fs';
+import { app, query } from 'mu';
+import xmlbuilder from 'xmlbuilder';
+
+const sitemapPath = 'sitemap.xml';
+
+app.get('/sitemap.xml', async function( req, res, next ) {
+  try {
+    if (fs.existsSync(sitemapPath)) {
+    // if file exist
+      returnSitemap(res);
+    }
+    else {
+      // else build file and return
+      const urls = await querySitemapResources();
+      await buildSitemap(urls);
+      returnSitemap(res);
+    }
+
+  }
+  catch(e) {
+    return next(new Error(e.message));
+  }
+});
+
+function returnSitemap(res) {
+  var stat = fs.statSync(sitemapPath);
+  res.set('Content-Type', 'application/xml');
+  res.set('Content-Length', stat.size);
+  const stream = fs.createReadStream(sitemapPath);
+  stream.pipe(res);
+}
+async function querySitemapResources() {
+  const queryString = fs.readFileSync('/config/query.rq').toString('utf-8');
+
+  const result = await query(queryString);
+  return result.results.bindings.map((row) => row['url'].value);
+}
+
+async function buildSitemap(urls) {
+  const xml = xmlbuilder.create('urlset', {}, {}, {separateArrayItems: true})
+          .att('xmlns', "http://www.sitemaps.org/schemas/sitemap/0.9");
+  for (var url of urls) {
+    xml.ele({url: {loc: url}});
+  }
+  const output = xml.end({pretty: true});
+  await new Promise(function (resolve, reject) {
+    fs.writeFile(sitemapPath, output, (err) => {
+      if (err)
+        reject(err);
+      else
+        resolve();
+    });
+  });
+}
